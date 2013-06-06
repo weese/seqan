05e309a139ffad130603a94d12fc90d76b5d4afe
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2013-06-06 12:26:44 +0200

    [INTERNAL] CUDA Mapper: restored overloaded device fibres

--- extras/apps/cuda_mapper/index.h
+++ extras/apps/cuda_mapper/index.h
@@ -124,14 +124,15 @@ struct Size<RankDictionary<TwoLevels<bool, TSpec> > >
     typedef __uint32 Type;
 };
 
-//template <typename TValue, typename TSpec>
-//struct RankDictionaryBitsPerBlock_<TwoLevels<TValue, Device<TSpec> > > :
-//    BitsPerValue<__uint32> {};
-//
-//template <typename TValue, typename TSpec>
-//struct RankDictionaryBitsPerBlock_<TwoLevels<TValue, View<TSpec> > > :
-//    BitsPerValue<__uint32> {};
+#ifdef __CUDACC__
+template <typename TValue, typename TSpec>
+struct RankDictionaryBitsPerBlock_<TwoLevels<TValue, Device<TSpec> > > :
+    BitsPerValue<__uint32> {};
 
+template <typename TValue, typename TSpec>
+struct RankDictionaryBitsPerBlock_<TwoLevels<TValue, View<TSpec> > > :
+    BitsPerValue<__uint32> {};
+#endif
 }
 
 #endif  // #ifndef SEQAN_EXTRAS_CUDAMAPPER_INDEX_H_
