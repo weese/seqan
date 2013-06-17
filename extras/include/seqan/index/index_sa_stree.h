c8bc806418390db85253b0ac543481e48d301ecc
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2013-06-17 23:52:27 +0200

    [NOP] CUDA: updated copyright notice

--- extras/include/seqan/index/index_sa_stree.h
+++ extras/include/seqan/index/index_sa_stree.h
@@ -3,6 +3,7 @@
 // ==========================================================================
 // Copyright (c) 2006-2013, Knut Reinert, FU Berlin
 // Copyright (c) 2013 NVIDIA Corporation
+// Copyright (c) 2013 NVIDIA Corporation
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
