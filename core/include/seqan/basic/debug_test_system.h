2693cf603e12ecb129f3566bc34582e6ece04335
Enrico Siragusa <enrico.siragusa@fu-berlin.de>
2013-07-09 17:13:46 +0200

    [FEATURE] Debug System: added device side assertions

--- core/include/seqan/basic/debug_test_system.h
+++ core/include/seqan/basic/debug_test_system.h
@@ -2176,7 +2176,27 @@ SEQAN_ASSERT_IN_DELTA_MSG(1, 0, 0.1, "msg");  // will fail with message
     } while (false)
 
 
-#else  // #if SEQAN_ENABLE_DEBUG && !defined(__CUDA_ARCH__)
+#elif SEQAN_ENABLE_DEBUG && defined(__CUDA_ARCH__)
+
+#define SEQAN_ASSERT_EQ(_arg1, _arg2) do { assert(_arg1 == _arg2); } while (false)
+#define SEQAN_ASSERT_EQ_MSG(_arg1, _arg2, ...) do { assert(_arg1 == _arg2); } while (false)
+#define SEQAN_ASSERT_NEQ(_arg1, _arg2) do { assert(_arg1 != _arg2); } while (false)
+#define SEQAN_ASSERT_NEQ_MSG(_arg1, _arg2, ...) do { assert(_arg1 != _arg2); } while (false)
+#define SEQAN_ASSERT_LEQ(_arg1, _arg2) do { assert(_arg1 <= _arg2); } while (false)
+#define SEQAN_ASSERT_LEQ_MSG(_arg1, _arg2, ...) do { assert(_arg1 <= _arg2); } while (false)
+#define SEQAN_ASSERT_LT(_arg1, _arg2) do { assert(_arg1 < _arg2); } while (false)
+#define SEQAN_ASSERT_LT_MSG(_arg1, _arg2, ...) do { assert(_arg1 < _arg2); } while (false)
+#define SEQAN_ASSERT_GEQ(_arg1, _arg2) do { assert(_arg1 >= _arg2); } while (false)
+#define SEQAN_ASSERT_GEQ_MSG(_arg1, _arg2, ...) do { assert(_arg1 >= _arg2); } while (false)
+#define SEQAN_ASSERT_GT(_arg1, _arg2) do { assert(_arg1 > _arg2); } while (false)
+#define SEQAN_ASSERT_GT_MSG(_arg1, _arg2, ...) do { assert(_arg1 > _arg2); } while (false)
+#define SEQAN_ASSERT(_arg1) do { assert(_arg1); } while (false)
+#define SEQAN_ASSERT_MSG(_arg1, ...) do { assert(_arg1); } while (false)
+#define SEQAN_ASSERT_NOT(_arg1) do { assert(!_arg1); } while (false)
+#define SEQAN_ASSERT_NOT_MSG(_arg1, ...) do { assert(!_arg1); } while (false)
+#define SEQAN_ASSERT_FAIL(...) do { assert(false); } while (false)
+
+#else
 
 #define SEQAN_ASSERT_EQ(_arg1, _arg2) do {} while (false)
 #define SEQAN_ASSERT_EQ_MSG(_arg1, _arg2, ...) do {} while (false)
