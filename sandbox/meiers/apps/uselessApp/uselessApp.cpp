0f549f27fd021c379482ceddc30b5643631a2b13
Sascha Meiers <meiers@inf.fu-berlin.de>
2014-02-03 13:57:43 +0100

    benchmark cpp for gapped tupler

--- sandbox/meiers/apps/uselessApp/uselessApp.cpp
+++ sandbox/meiers/apps/uselessApp/uselessApp.cpp
@@ -34,20 +34,138 @@
 
 
 #include <seqan/sequence.h>
+#include <seqan/arg_parse.h>
 #include <seqan/seq_io.h>
 #include <seqan/index.h>
 
 using namespace seqan;
 
-int main()
+// --------------------------------------------------------------------------
+// Class AppOptions
+// --------------------------------------------------------------------------
+
+struct AppOptions
 {
+    seqan::String<char> infile;
+    seqan::String<char> outfile;
+};
 
-    StringSet<CharString> ids;
-    StringSet<Dna5String> seqs;
+// ==========================================================================
+// Functions
+// ==========================================================================
 
-    /*
+// --------------------------------------------------------------------------
+// Function parseCommandLine()
+// --------------------------------------------------------------------------
+
+seqan::ArgumentParser::ParseResult
+parseCommandLine(AppOptions & options, int argc, char const ** argv)
+{
+    // Setup ArgumentParser.
+    seqan::ArgumentParser parser("seqanBuildSuffixArray");
+    // Set short description, version, and date.
+    setShortDescription(parser, "Builds an ESA-index.");
+    setVersion(parser, "Sascha.0.1");
+    setDate(parser, "July 2013");
+
+    // Define usage line and long description.
+    addUsageLine(parser, "[\\fIOPTIONS\\fP] \"\\fITEXT\\fP\"");
+    addDescription(parser, "Builds an index (ESA) of the specified genome and writes it to disk.");
+
+    // We require one argument.
+    addArgument(parser, seqan::ArgParseArgument(seqan::ArgParseArgument::INPUTFILE, "IN"));
+
+    addOption(parser, seqan::ArgParseOption("o",
+                                            "output",
+                                            "Output file (<IN>.index if not specified)",
+                                            seqan::ArgParseArgument::OUTPUTFILE, "OUT"));
+
+    // Parse command line.
+    seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
+
+    // Only extract  options if the program will continue after parseCommandLine()
+    if (res != seqan::ArgumentParser::PARSE_OK)
+        return res;
+
+    // set outfile to infile.db if not outfile was specified
+    seqan::String<char> outfile;
+    seqan::getArgumentValue(options.infile, parser, 0);
+
+    outfile = options.	infile;
+    outfile += ".index";
+    getOptionValue(outfile, parser, "output");
+    options.outfile = outfile;
+
+    return seqan::ArgumentParser::PARSE_OK;
+}
+
+
+// --------------------------------------------------------------------------
+// Function callBenchmark()
+// --------------------------------------------------------------------------
+
+template <typename TString, typename TSpec>
+void callBenchmarks(StringSet<TString, TSpec> const & set) {
+
+    CharString dislexExt = "Extern";
+    CharString shapeStr  = "10011001100";
+    CharString string    = "String";
+    CharString stringset = "StrSet";
+
+    //- Gapped Indices: 10011001100 ----------------------------------------------------------------
+
+    typedef CyclicShape<FixedShape<0,GappedShape<HardwiredShape<2,1,3,1> >, 2> > TShape;
+
+    {
+        typedef typename Concatenator<StringSet<TString, TSpec> >::Type			TConcat;
+        typedef Multi<GappedTupler<TShape, false>,
+                Pair<typename Size<TString>::Type>,
+                typename StringSetLimits<StringSet<TString, TSpec> >::Type>     TMulti;
+
+        // specialization
+		typedef Pipe< TConcat, Source<> >                                       TSource;
+        typedef Pipe< TSource, TMulti>                                          TTupler;
+
+        TSource source(concat(set));
+        TTupler tupler(source, stringSetLimits(set));
+        beginRead(tupler);
+        while (!eof(tupler))
+            pop(tupler);
+        endRead(tupler);
+    }
+
+}
+
+// --------------------------------------------------------------------------
+// Function main()
+// --------------------------------------------------------------------------
+
+// Program entry point.
+
+int main(int argc, char const ** argv)
+{
+    // Parse the command line.
+    seqan::ArgumentParser parser;
+    AppOptions options;
+    seqan::ArgumentParser::ParseResult res = parseCommandLine(options, argc, argv);
+
+    // If there was an error parsing or built-in argument parser functionality
+    // was triggered then we exit the program.  The return code is 1 if there
+    // were errors and 0 if there were none.
+    if (res != seqan::ArgumentParser::PARSE_OK)
+        return res == seqan::ArgumentParser::PARSE_ERROR;
+
+
+    // start main program ///////////////////////////////////////////////////
+
+    seqan::StringSet<seqan::CharString> ids;
+    seqan::StringSet<seqan::Dna5String> seqs;
+
+
+
+    // New reading:
     // use the sequenceStream
-    SequenceStream seqStream(argv[1]);
+    seqan::SequenceStream seqStream(seqan::toCString(options.infile));
     if (!isGood(seqStream))
     {
         std::cerr << "ERROR: Could not open the file." << std::endl;
@@ -55,59 +173,17 @@ int main()
     }
     if (readAll(ids, seqs, seqStream) != 0)
     {
-        std::cerr << "ERROR: Could not read from file" << std::endl;
+        std::cerr << "ERROR: Could not read from " << options.infile << std::endl;
         return 1;
     }
-     */
 
 
-    // variable typedefs:
-    typedef CyclicShape<FixedShape<0, GappedShape<HardwiredShape<2> >, 0> > TShape;
-    typedef GappedTupler<TShape, false> GappedTupler_;
+    time_t t; time(&t);
+    std::cout << "# input file : " << options.infile << std::endl;
+    std::cout << "# timestamp  : " << ctime(&t);
 
-    CharString shapeString;
-    cyclicShapeToString(shapeString, TShape());
-    std::cout << "Shape: " << shapeString << std::endl << std::endl;
+    callBenchmarks(seqs);
 
-    {
-        // StringSet
-        std::cout << "StringSet" << std::endl;
-        StringSet<CharString> set;
-        appendValue(set, "0123456789");
-        appendValue(set, "012");
-        appendValue(set, "0");
-        appendValue(set, "0123456789");
-        appendValue(set, "012");
-
-        typedef Concatenator<StringSet<CharString> >::Type      TConcat;
-        typedef StringSetLimits<StringSet<CharString> >::Type   TLimits;
-		typedef Multi<DislexExternal<TShape>, Pair<unsigned>, TLimits>  TMulti;
-        typedef Pipe<TConcat, Source<> >                        TPipeSource;
-
-        typedef Pipe<TPipeSource, TMulti >                      TPipeDislex;
-        TPipeSource source(concat(set));
-        TPipeDislex dislex(source, stringSetLimits(set));
-
-        String<Pair<unsigned> > sa;
-        sa << dislex;
-        std::cout << sa;
-    }
-
-    {
-        // String
-        std::cout << "String" << std::endl;
-        CharString s = "banaaanaaa";
-
-        typedef Pipe< CharString, Source<> >                    TPipeSource;
-        typedef Pipe< TPipeSource, DislexExternal<TShape> >     TPipeDislex;
-        TPipeSource source(s);
-        TPipeDislex dislex(source);
-
-        String<unsigned> sa;
-        sa << dislex;
-        std::cout << sa;
-    }
-
-    
+    return 0;
 }
 
